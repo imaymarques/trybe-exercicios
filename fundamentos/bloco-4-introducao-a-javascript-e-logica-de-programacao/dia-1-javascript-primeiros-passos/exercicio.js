d4f9e6f006b0d22236af9f8a0a52e081d42a8b53
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-12 16:58:04 -0300

    Adicionando exercício 5 do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -1,12 +1,17 @@
-/*4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+/*5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+--> Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+--> Um ângulo será considerado inválido se não tiver um valor positivo.*/
 
-const valor = 27;
-if(valor > 0){
-    console.log("Positivo")
+const angulo1 = 90;
+const angulo2 = -30;
+const angulo3 = -10;
+
+if(angulo1 + angulo2 + angulo3 === 180 && angulo1 + angulo2 + angulo3 > 0){
+    console.log(true)
 }
-else if(valor < 0){
-    console.log("Negativo")
+else if (angulo1 + angulo2 + angulo3 !== 180 && angulo1 + angulo2 + angulo3 > 0){
+    console.log(false)
 }
-else{
-    console.log("Zero")
+else {
+    console.log("Erro, ângulo inválido!")
 }
