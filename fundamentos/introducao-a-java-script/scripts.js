b1dc148dd5e284c77101ccb2ef41459c84fba584
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-12 12:12:11 -0300

    Adicionando exercícios Javascript

---
+++ fundamentos/introducao-a-java-script/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Mayara";
+const birthCity = "Recife";
+let birthYear = 1994;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Jaboatão"
+console.log(birthCity);
