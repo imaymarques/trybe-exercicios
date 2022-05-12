c1da445109ad0056483ef86d2626c914542cfa64
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-12 18:48:17 -0300

    Adicionando o sétimo exercício do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -1,15 +1,32 @@
-/*6. Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
-Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
-Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case).
-Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
-Exemplo: bishop (bispo) -> diagonals (diagonais)
+/*7. Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
 */
 
-let xadrez = "Cavalo";
-
-if (xadrez === "Cavalo"){
-    console.log("Movimento em L" .toLowerCase())
+let porcentagem = "90";
+if (porcentagem >= 90 && porcentagem <= 100){
+    console.log("A")
+}
+else if (porcentagem >= 80 && porcentagem < 90){
+    console.log("B")
+}
+else if (porcentagem >= 70 && porcentagem < 80){
+    console.log("C")
+}
+else if (porcentagem >= 60 && porcentagem < 70){
+    console.log("D")
+}
+else if (porcentagem >= 50 && porcentagem < 60){
+    console.log("E")
+}
+else if (porcentagem >=0 && porcentagem < 50){
+    console.log("F")
 }
 else{
-    console.log("Erro, peça inválida!")
+    console.log("Erro, essa nota não existe!")
 }
