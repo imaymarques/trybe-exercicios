817cca0f9c3eabfa469e6a11c18449280689dded
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-13 20:15:01 -0300

    Adicionando o quarto exercício do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,4 +1,4 @@
-/*3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array; */
+/*4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"; */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
@@ -6,3 +6,9 @@ for (let index = 0; index < numbers.length; index += 1){
     soma = soma + numbers[index] / numbers.length;
 }
 console.log(soma);
+if (soma > 20){
+    console.log("Valor maior que 20!")
+}
+else{
+    console.log("Valor menor que 20!")
+}
