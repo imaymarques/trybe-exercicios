9c02a0fd01f43ba537dc80cd995a40e475e33ec9
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-13 21:41:19 -0300

    Adicionando o quinto exercício do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,14 +1,10 @@
-/*4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"; */
+/*5. Utilizando for, descubra qual o maior valor contido no array e imprima-o; */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let num = [0];
 for (let index = 0; index < numbers.length; index += 1){
-    soma = soma + numbers[index] / numbers.length;
+    if(numbers[index] > num){
+        num = numbers[index]
+    }
 }
-console.log(soma);
-if (soma > 20){
-    console.log("Valor maior que 20!")
-}
-else{
-    console.log("Valor menor que 20!")
-}
+console.log(num)
