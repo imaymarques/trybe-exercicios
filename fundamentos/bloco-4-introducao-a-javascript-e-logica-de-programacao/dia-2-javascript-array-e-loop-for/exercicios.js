8a0f80d5b87834b27a70750dadff8cb134e32593
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-14 17:54:58 -0300

    Adicionando o nono exercício do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,9 +1,11 @@
-/*8. Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado; */
+/*9. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2. */
 
 let num = [];
 
 for (let index = 0; index <= 25; index += 1){
     num.push(index);
 }
-console.log(num);
-
+for (let div of num){
+    div = div / 2;
+    console.log(div)
+}
