fe83c28814b939ad64676b2526807378a045f5c8
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-15 13:52:02 -0300

    Adicionando o terceiro exercício do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,9 +1,18 @@
-/* 1. O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo: O fatorial é representado pelo sinal !
-4! = 4 x 3 x 2 x 1 = 24
-Com base nessas informações, crie um algoritmo que retorne o fatorial de 10. */
+/* 3. Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra. */
 
-let fatorial = 10;
-for (let index = 1; index <= 10; index += 1){
-    fatorial = fatorial * index
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let big = [0];
+let small = array[0];
+
+for (let index = 0; index < array.length; index += 1){
+    if (array[index].length > big.length){
+        big = array[index];
+    }
 }
-console.log(fatorial);
+for (let index = 0; index < array.length; index += 1){
+    if (array[index].length < small.length){
+        small = array[index];
+    }
+}
+console.log('A maior palavra é: ', big);
+console.log('A menor palavra é: ', small);
